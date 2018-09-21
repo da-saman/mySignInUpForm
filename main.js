ef1395508b39799de80e0bb6d3cf12f69eb859f5
saman darabi <darabi.s.i@gmail.com>
2018-09-21 10:19:47 +0430

    a goog stage, sign in and sign up form works good, text on button is animatted

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ $(document).ready(function () {
 		$('form').fadeOut(500);
 		$('.wrapper').addClass('form-success');
 	});
-	$(".signupLink").click(function(){
-		console.log("OOOOOOOOOOK");
-		$(".signup").toggleClass("show");
+	$(".signupLink").click(function () {
+		$(".signup").slideToggle();
+		$('form button').toggleClass('clicked'); 
+		console.log($("#inpttxt").height());
+		// var el = $("form button");
+		// el.text() == el.data("text-original")
+		// 	? el.text(el.data("text-swap"))
+		// 	: el.text(el.data("text-original"));
 	});
 });
